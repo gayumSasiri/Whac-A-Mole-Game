1a26446068c546c76c723615b3cd3c20d247614d
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-12-25 08:09:28 +0530

    mole tile and piranh plant tile placement implimented

---
+++ main.js
@@ -0,0 +1,56 @@
+let currMoleTile;
+let currPlantTile; 
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    for(let i = 0; i < 9; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMole, 1000);
+    setInterval(setPlant, 2000);
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    if(currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "./monty-mole.png";
+
+    let num = getRandomTile();
+    if ( currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if(currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./piranha-plant.png";
+
+    let num = getRandomTile();
+    if( currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
